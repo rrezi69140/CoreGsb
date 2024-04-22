b29b62b84805dd085f832744ff2d8900aa60c4aa
rachid rezig <rachid.rezig@lmdsio.fr>
2024-04-22 14:15:52 +0200

    Ajoutez des fichiers projet.

---
+++ CoreGsb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreGsb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
